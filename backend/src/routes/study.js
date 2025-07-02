3a9d4d2602b1a7154ae4bca9472729e1c2cff218
100ke <bhy9803@gmail.com>
2025-07-03 00:36:29 +0900

    스터디 CRUD 구현, swagger 추가

--- backend/src/routes/study.js
+++ backend/src/routes/study.js
@@ -4,5 +4,9 @@ const studyController = require("../controllers/studyController");
 const { authenticate } = require("../middlewares/authMiddleware");
 
 router.post("/", authenticate, studyController.createStudy);
+router.get("/", studyController.readStudyList);
+router.get("/:id", studyController.readStudy);
+router.patch("/:id", studyController.updateStudy);
+router.delete("/:id", studyController.deleteStudy);
 
 module.exports = router;
